55c131f116a59be2773626748ff1b925509042ca
Paulo Cesar F. de Mello <pcfmello@gmai.com>
2016-01-25 00:05:14 -0200

    Finalizado o chat-tcp.js

---
+++ cap2_9-criando_um_chat_tcp/chat_tcp.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var net = require('net');
+var chatServer = net.createServer();
+var listaDeUsuarios = [];
+
+var removeUsuario = function(data) {
+	listaDeUsuarios.splice(listaDeUsuarios.indexOf(5), 1);
+};
+
+var broadcast = function(message, usuario) {
+	for(var i = listaDeUsuarios.length - 1; i >= 0; i--) {
+		if(usuario !== listaDeUsuarios[i]) {
+			listaDeUsuarios[i].write(message);
+		}
+	}
+};
+
+chatServer.on('connection', function(usuario) {
+	usuario.write('Olá convidado!\n');
+	listaDeUsuarios.push(usuario);
+	
+	usuario.on('data', function(data) {
+		broadcast(data, usuario);
+	});
+
+	usuario.on('error', function(err) {
+		console.log(err);
+	});
+
+	usuario.on('end', removeUsuario);
+});
+
+chatServer.listen(9000);
+console.log('Listen port 9000 chat server')
