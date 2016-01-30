c654d8e4e8a2bbd22842f89cf26e0ebc8759f9e9
Paulo Cesar F. de Mello <pcfmello@gmai.com>
2016-01-30 18:15:33 -0200

    Criado outro app.js para o cap 5.5.4

--- app_cap5_5_4_middleware_erros.js
+++ app_cap5_5_4_middleware_erros.js
@@ -7,61 +7,25 @@
 var express = require('express');
 var app = express();
 
-/**
- * @module  Method Override
- * @install $npm install method-override --save
- * @about   Faz a aplicação entender outros verbos além de GET e POST.
- * */
-var methodOverride = require('method-override');
-
-/**
- * @module  Body Parser
- * @install $ npm install body-parser --save
- * @about   Módulo utilizado para que o servidor entenda os formatos
- *          query string e JSON das requisições, que por padrão, 
- *          só entende texto puro.
- * */
-var bodyParser = require('body-parser');
- 
-//Formas que a aplicação os verbos além de GET e POST
-app.use(methodOverride('X-HTTP-Method'));
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(methodOverride('X-Method-Override'));
-app.use(methodOverride('_method')); //Forma query string
-
-//Configura o servidor para trabalhar com a API REST
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-//Middleware para "Não servir o arquivo favicon.ico"
-app.use(function(request, response, next) {
-	if(request.url === '/favicon.ico') {		
-		response.writeHead(200, {'Content-Type': 'image/x-icon'});
-		response.end();
-	} else {
-		next();
-	}
-});
-
-app.get('/', function(req, res) {
-	res.send('Hello World!');
-});
-
 app.get('/contato', function(req, res) {
-	res.status(200);
-	if(req.accepts('text')) {
-		res.write('Nome: Paulo Cesar\n');
-		res.write('E-mail: pcfmello@gmail.com');
-		res.end();
-	} else {
-		res.json({nome: 'Paulo Cesar', email: 'pcfmello@gmail.com', cidade: 'Florianópolis'});
-	}
+	res.status(201);
+	res.json({nome: 'Paulo Cesar', email: 'pcfmello@gmail.com', cidade: 'Florianópolis'});
 });
 
+app.use(function(request, response, next) {
+	var err = new Error('Recurso não encontrado');
+	err.status = 404;
+	next(err);
+});
 
+app.use(function(err, request, response, next) {
+	console.log(err.stack);
+	response.status(err.status || 500).json({err: err.message});
+});
 
 var server = app.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
+	console.log('Exemplo de aplicação RESTful escutando em http://'.concat(host) + ':'.concat(port));
 	console.log('Exemplo de aplicação RESTful escutando em http://%s:%s', host, port);
-})
+});
